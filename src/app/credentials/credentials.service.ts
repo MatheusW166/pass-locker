c908b3a8cde10656f477c8b70e5eab526f34b894
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-09-03 16:33:23 -0400

    feat: encrypts credential password

--- src/app/credentials/credentials.service.ts
+++ src/app/credentials/credentials.service.ts
@@ -1,11 +1,12 @@
 import {
   ConflictException,
+  ForbiddenException,
   Injectable,
   NotFoundException,
-  UnauthorizedException,
 } from '@nestjs/common';
 import { CreateCredentialDto } from './dto/create-credential.dto';
 import { CredentialsRepository } from './credentials.repository';
+import Cryptr from 'cryptr';
 
 @Injectable()
 export class CredentialsService {
@@ -17,14 +18,20 @@ export class CredentialsService {
       userId,
     );
     if (credential) throw new ConflictException();
+
+    const { password } = createCredentialDto;
+    const crypt = new Cryptr(process.env.JWT_SECRET);
+    createCredentialDto.password = crypt.encrypt(password);
     return this.credentialsRepository.create(createCredentialDto, userId);
   }
 
   async findByIdOrThrow(id: number, userId: number) {
     const credential = await this.credentialsRepository.findById(id);
     if (!credential) throw new NotFoundException();
-    if (credential.userId !== userId) throw new UnauthorizedException();
-    return credential;
+    if (credential.userId !== userId) throw new ForbiddenException();
+
+    const crypt = new Cryptr(process.env.JWT_SECRET);
+    return { ...credential, password: crypt.decrypt(credential.password) };
   }
 
   async remove(id: number, userId: number) {
